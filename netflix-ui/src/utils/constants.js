3e6b0438c279bc028af8330b5368a63f0c61a3cd
Rahul Rajput <rahulnandola143@gmail.com>
2023-12-08 01:46:16 +0530

    changes

--- netflix-ui/src/utils/constants.js
+++ netflix-ui/src/utils/constants.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 export const API_KEY = "94eaa673cf0ead18963c6deb2a7a4db1";
 
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 
-export const BASE_URL = "http://localhost:5000";
-=======
-export const API_KEY = "94eaa673cf0ead18963c6deb2a7a4db1";
-
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-
-export const BASE_URL = "http://localhost:5000";
->>>>>>> 0a0e3d3934689a46960219414c2d33bce0c53a34
+export const BASE_URL = "https://netflixbackend-7dqr.onrender.com";
